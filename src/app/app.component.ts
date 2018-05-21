3db0cea2d1d468ed6e9c135c7b71a9df17c8e05f
Luis Felipe Chica <lchica@lchicamac.local>
2018-05-20 23:56:08 -0500

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-	title = 'Bienvenido a la patria del platanal!';
-}
+export class AppComponent {}
